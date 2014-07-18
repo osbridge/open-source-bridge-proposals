fd0fe0ee96dff3bc8eb7e27dcfbeacf5c558b3f4
Reid Beels <reid.beels@elementaltechnologies.com>
2014-07-18 09:26:19 -0700

    Add explicit conversion of rack-robustness errors to UTF-8

--- config/application.rb
+++ config/application.rb
@@ -27,8 +27,8 @@ module OpenSourceBridgeProposals
       g.no_catch_all
       g.on(ArgumentError) { |ex| 400 }
       g.content_type 'text/plain'
-      g.body{ |ex| ex.message }
-      g.ensure(true) { |ex| env['rack.errors'].write(ex.message) }
+      g.body{ |ex| ex.message.to_s.encode('UTF-8', {:invalid => :replace, :undef => :replace, :replace => '?'}) }
+      g.ensure(true) { |ex| env['rack.errors'].write(ex.message.to_s.encode('UTF-8', {:invalid => :replace, :undef => :replace, :replace => '?'})) }
     end
   end
 end
